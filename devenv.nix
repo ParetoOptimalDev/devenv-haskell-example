2620383bb1117296d1572fe59286401b0aab6d7c
ParetoOptimalDev <pareto.optimal@mailfence.com>
2023-07-26 10:42:20 -0500

    update to use amazonka 2.0
    Well... try to
    
    https://github.com/cachix/devenv/issues/754
    https://github.com/brendanhay/amazonka/releases/tag/2.0.0-rc2

--- devenv.nix
+++ devenv.nix
@@ -1,10 +1,22 @@
-{ pkgs, ... }:
+{ pkgs, inputs, ... }:
 
+let myHaskellPackages = 
+      pkgs.haskell.packages.ghc96.override (old: {
+        overrides = pkgs.lib.composeExtensions (old.overrides or (_: _: {})) 
+          (hself: hsuper: {
+            ghc = hsuper.ghc // { withPackages = hsuper.ghc.withHoogle; };
+            ghcWithPackages = hself.ghc.withPackages;
+
+            amazonka = hself.callCabal2Nix "amazonka" "${inputs.amazonka}/lib/amazonka";
+            amazonka-core = hself.callCabal2Nix "amazonka-core" "${inputs.amazonka}/lib/amazonka-core";
+          });
+      });
+in
 {
 
   languages.haskell = {
     enable = true;
-    package = pkgs.haskell.packages.ghc96.ghc;
+    package = myHaskellPackages.ghc;
     languageServer = null;
   };
 
